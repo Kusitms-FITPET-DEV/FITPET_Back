3c13eccca5cae52972ec267dd31f03ca17a91268
jihyun <chiiclaro@gmail.com>
2024-08-23 14:40:41 +0900

    first commit

---
+++ src/main/java/appjjang/fitpet/FitpetApplication.java
@@ -0,0 +1,13 @@
+package appjjang.fitpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appjjang/fitpet/FitpetApplicationTests.java
@@ -0,0 +1,13 @@
+package appjjang.fitpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
