b07c81e38ca6f3e65bdc01fbff2072420534efc3
iiqcov <iiqcov@gmail.com>
2024-08-24 00:53:47 +0900

    [Chore] BaseTimeEntity 추가

---
+++ src/main/java/appjjang/fitpet/global/config/jpa/JpaConfig.java
@@ -0,0 +1,8 @@
+package appjjang.fitpet.global.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
