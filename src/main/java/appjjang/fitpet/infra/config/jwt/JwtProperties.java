0031e13c62a275eccfac506cc55610881672238b
jihyun <chiiclaro@gmail.com>
2024-08-31 14:20:38 +0900

    [Chore] 환경변수 설정

--- src/main/java/appjjang/fitpet/infra/config/jwt/JwtProperties.java
+++ src/main/java/appjjang/fitpet/infra/config/jwt/JwtProperties.java
@@ -3,7 +3,9 @@ package appjjang.fitpet.infra.config.jwt;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "jwt")
 @Getter
 @Setter
