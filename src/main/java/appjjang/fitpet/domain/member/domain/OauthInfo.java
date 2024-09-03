3464aa148c66ac96e19d722602106ddd87c4913c
iiqcov <iiqcov@gmail.com>
2024-09-03 19:10:15 +0900

    [Feat] 카카오 소셜 로그인 시 이메일 및 프로필, 닉네임 추가

--- src/main/java/appjjang/fitpet/domain/member/domain/OauthInfo.java
+++ src/main/java/appjjang/fitpet/domain/member/domain/OauthInfo.java
@@ -9,6 +9,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 import static appjjang.fitpet.domain.member.domain.OauthProvider.KAKAO;
+import static appjjang.fitpet.global.common.constants.SecurityConstants.NICKNAME;
+import static appjjang.fitpet.global.common.constants.SecurityConstants.PICTURE;
+
 @Embeddable
 @Getter
 @NoArgsConstructor
@@ -18,18 +21,26 @@ public class OauthInfo {
     @Enumerated(EnumType.STRING)
     private OauthProvider provider;
 
+    private String profileUrl;
+    private String nickname;
     private String email;
 
     @Builder
-    public OauthInfo(String oauthId, OauthProvider provider) {
+    public OauthInfo(String oauthId, OauthProvider provider, String profileUrl, String nickname, String email) {
         this.oauthId = oauthId;
         this.provider = provider;
+        this.profileUrl = profileUrl;
+        this.nickname = nickname;
+        this.email = email;
     }
 
     public static OauthInfo from(OidcUser user) {
         return OauthInfo.builder()
                 .oauthId(user.getSubject())
                 .provider(KAKAO)
+                .profileUrl(user.getAttributes().get(PICTURE).toString())
+                .nickname(user.getClaim(NICKNAME))
+                .email(user.getEmail())
                 .build();
     }
 }
