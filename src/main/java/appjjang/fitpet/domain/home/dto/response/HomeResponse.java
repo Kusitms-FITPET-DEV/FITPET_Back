5cff532b70f9e364a144e966a9effb519992a10e
iiqcov <iiqcov@gmail.com>
2024-09-07 19:37:10 +0900

    [Feat] 홈 조회 로직에서 배너 제시 제거

--- src/main/java/appjjang/fitpet/domain/home/dto/response/HomeResponse.java
+++ src/main/java/appjjang/fitpet/domain/home/dto/response/HomeResponse.java
@@ -1,6 +1,5 @@
 package appjjang.fitpet.domain.home.dto.response;
 
-import appjjang.fitpet.domain.home.dto.BannerListDto;
 import appjjang.fitpet.domain.home.dto.JournalListDto;
 import appjjang.fitpet.domain.home.dto.QuestionDto;
 import appjjang.fitpet.domain.home.dto.QuizDto;
@@ -12,7 +11,6 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class HomeResponse {
-    private BannerListDto bannerList;
     private List<JournalListDto> journalList;
     private QuizDto quiz;
     private QuestionDto questionList;
